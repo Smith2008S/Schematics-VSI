bb5ae94f287660a8c2f6005db25290e00c37cbc8
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-04-13 10:16:44 -0500

    Update files

--- vars.tf
+++ vars.tf
@@ -2,6 +2,9 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 }
 
+variable "iaas_classic_api_key" { }
+variable "iaas_classic_username" { }
+
 variable "os_reference" {
   description = ""
   default = "UBUNTU_18_64"
