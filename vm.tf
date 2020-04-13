3801cc7f22c0451c4c3863c2c4f5bae3e52cf7d8
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-04-13 10:43:13 -0500

    Update files3

--- vm.tf
+++ vm.tf
@@ -2,8 +2,6 @@ provider "ibm" {
   iaas_classic_username = ${var.iaas_classic_username}
   iaas_classic_api_key  = ${var.iaas_classic_api_key}
   ibmcloud_api_key   = ${var.ibmcloud_api_key}
-  generation         = 1
-  region             = "us-south"
 }
 
 
