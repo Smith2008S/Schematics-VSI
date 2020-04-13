662780b921ed017c3636ff7ed5bdd5ffcd0a193c
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-04-13 10:44:03 -0500

    Update file

--- vm.tf
+++ vm.tf
@@ -1,13 +1,7 @@
-provider "ibm" {
-  iaas_classic_username = ${var.iaas_classic_username}
-  iaas_classic_api_key  = ${var.iaas_classic_api_key}
-  ibmcloud_api_key   = ${var.ibmcloud_api_key}
-}
-
 
 resource "ibm_compute_vm_instance" "twc_terraform_sample" {
   hostname                   = "schematicsvsi"
-  domain                     = "schematicsvsi.com"
+  domain                     = "ibm.schematicsvsi.com"
   os_reference_code          = "${var.os_reference}"
   datacenter                 = "${var.datacenter}"
   network_speed              = 10
