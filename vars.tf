68fff6ba2a06ada092526ad41b6f5cedbdccdb07
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-04-13 09:45:25 -0500

    Update files3

--- vars.tf
+++ vars.tf
@@ -2,10 +2,6 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 }
 
-variable "resource_group" {
-  description = "Name of resource group to provision resources"
-}
-
 variable "os_reference" {
   description = ""
   default = "UBUNTU_18_64"
